8e0def3b2a568c33be0c135c861948806803480b
Swapnil <swapniljain240@gmail.com>
2022-03-06 15:47:53 +0530

    done with frontent part

--- src/chat.js
+++ src/chat.js
@@ -1,11 +1,80 @@
 import React from 'react'
 import './css/chat.css'
+import { Avatar, IconButton } from '@mui/material';
+
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import MicIcon from '@mui/icons-material/Mic';
 function Chat() {
 
     return (
         <div className='chat'>
 
-            <hq> hey i am a Chat</hq>
+            <div className='chat_header'>
+
+                <Avatar />
+
+                <div className='chat_headerInfo'>
+                    <h3>Room name</h3>
+                    <p>Last seen at ....</p>
+
+                </div>
+
+                <div className='chat_headerRight'>
+                    <IconButton>
+                        <SearchRoundedIcon />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+
+                </div>
+            </div>
+
+            <div className='chat_body'>
+                <p className='chat_message'>
+                    <span className='chat_name'>
+                        Swap
+                    </span>
+                    This is a message
+                    <span className='chat_timestamp'>
+                        {new Date().toLocaleDateString()}
+                        {new Date().toLocaleTimeString()}
+                    </span>
+                </p>
+                <p className='chat_message chat_receiver'>
+                    <span className='chat_name'>
+                        Swap
+                    </span>
+                    This is a message
+                    <span className='chat_timestamp'>
+                        {new Date().toLocaleDateString()}
+                        {new Date().toLocaleTimeString()}
+                    </span>
+                </p>
+            </div>
+
+            <div className='chat_footer'>
+                <IconButton>
+                    <EmojiEmotionsIcon />
+                </IconButton>
+
+                <form>
+
+                    <input placeholder='Type a message' type='text' />
+                    <button type='submit'>Send </button>
+
+                </form>
+                <IconButton>
+                    <MicIcon />
+                </IconButton>
+            </div>
         </div>
 
     )
