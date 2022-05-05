057c2df7ae1083b08843e8218127a177b1135be1
Swapnil <swapniljain240@gmail.com>
2022-05-05 16:54:42 +0530

    setting up mongodb

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+
+//connect the database
+mongoose.connect('mongodb://localhost:27017/Whatsapp_Clone');
+
+//acquire connection (to check it successful)
+const db = mongoose.connection;
+
+//error
+db.on('error',console.log.bind(console,'error connecting to db'));
+
+
+//up and runnning then print the message
+db.once('open',function(){
+    console.log('successfullly connected to database');
+})
