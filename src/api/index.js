c436e02a0afabc867389d4251742806300e658c0
Swapnil <swapniljain240@gmail.com>
2022-05-13 18:31:26 +0530

    custom fetch and userLogin function

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,62 @@
-const customFetch = ()=>{
-    try{
-        const response = fetch(url,config)
+import API_URL from '../utils/constant' 
+const customFetch =async (url,config)=>{
+    const token = window.localStorage.getItem("LOCALSTORAGE_TOKEN_KEY");
+
+    var headers = {
+        'content-type': 'application/x-www-form-urlencoded',
+        Accept: 'application/json',
+
+    };
+
+    if (token) {
+        headers.Authentication = `Bearer ${token}`
     }
-    catch{
+    // var config = {
+    //     ...customConfig,
+    //     headers: {
+    //         ...headers,
+    //         ...customConfig.headers
+    //     }
+    // }
+    // if (body) {
+    //     config.body = getFormBody(body);
+    // }
+    try {
+        const response = await fetch(url, config);
+        const data = await response.json();
+
+        if (data.success) {
+            return {
+                data: data.data,
+                success: true
+            }
+        }
+        else {
+            throw new Error(data.message);
+        }
+    }
+    catch (error) {
+        console.log('Error in custom Fetch =>', error);
+        return {
+            message: error.message,
+            success: false
+        }
 
     }
+
 }
 
 export const getRoomsData = ()=>{
 
+}
+
+export const userLogin = (email,password)=>{
+    const response = customFetch(API_URL.signIn,{
+        method:'POST',
+        body:{
+            email:email,
+            password:password
+        }
+    })
+    return response
 }
---
+++ src/utils/constant.js
@@ -0,0 +1,6 @@
+const API_URL = {
+    signIn:"http://localhost:8000/v1/user/sign-in",
+    signUp:"http://localhost:8000/v1/user/sign-up"
+}
+
+export default API_URL;
